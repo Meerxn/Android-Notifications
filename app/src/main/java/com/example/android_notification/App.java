b461b62566601892e38f9e6bebcf79ebf5b0d31b
Meerxn <fardeenfmeeran@gmail.com>
2021-11-14 02:13:52 -0600

    Second Notification and Toast complete

--- app/src/main/java/com/example/android_notification/App.java
+++ app/src/main/java/com/example/android_notification/App.java
@@ -8,6 +8,7 @@ import android.os.Build;
 
 public class App extends Application {
     public static final String Channel_1_ID = "channel1";
+    public static final String Channel_2_ID = "channel2";
     @Override
     public void onCreate(){
         super.onCreate();
@@ -17,9 +18,13 @@ public class App extends Application {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
             NotificationChannel channel1 =  new NotificationChannel(Channel_1_ID, "Channel 1", NotificationManager.IMPORTANCE_HIGH);
+            NotificationChannel channel2 =  new NotificationChannel(Channel_2_ID, "Channel 2", NotificationManager.IMPORTANCE_LOW);
+
             channel1.setDescription("This is channel 1");
+            channel2.setDescription("This is channel 2");
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
 
         }
 
--- app/src/main/java/com/example/android_notification/MainActivity.java
+++ app/src/main/java/com/example/android_notification/MainActivity.java
@@ -6,12 +6,17 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static final String CHANNEL_1_ID = "channel1";
+    private static final String CHANNEL_2_ID = "channel2";
+
     private NotificationManagerCompat notificationManager;
     private EditText editTextTitle;
     private EditText editTextMessage;
@@ -29,13 +34,35 @@ public class MainActivity extends AppCompatActivity {
     public void sendOnChannel1(View v){
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
+        Intent activityIntent = new Intent(this,MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,0,activityIntent,0);
+        Intent broadcastIntent = new Intent (this,NotificationReciever.class);
+        broadcastIntent.putExtra("toastMessage",message);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this,0,broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT);
         Notification notifcation = new NotificationCompat.Builder(this,CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_baseline_chat_24)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .addAction(R.mipmap.ic_launcher,"Toast",actionIntent)
                 .build();
         notificationManager.notify(1,notifcation);
+    }
+    public void sendOnChannel2(View v){
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+        Notification notifcation = new NotificationCompat.Builder(this,CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_baseline_bluetooth_searching_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManager.notify(2,notifcation);
     }
 }
