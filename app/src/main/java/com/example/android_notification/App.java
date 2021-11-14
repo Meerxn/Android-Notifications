be652bd988e4f8058e62655e86908aa5a11c08c3
Meerxn <fardeenfmeeran@gmail.com>
2021-11-13 23:46:23 -0600

    First Notification Complete

---
+++ app/src/main/java/com/example/android_notification/App.java
@@ -0,0 +1,27 @@
+package com.example.android_notification;
+
+import android.app.Application;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class App extends Application {
+    public static final String Channel_1_ID = "channel1";
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        createNotificationsChannels();
+    }
+    public void createNotificationsChannels(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            NotificationChannel channel1 =  new NotificationChannel(Channel_1_ID, "Channel 1", NotificationManager.IMPORTANCE_HIGH);
+            channel1.setDescription("This is channel 1");
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/android_notification/MainActivity.java
+++ app/src/main/java/com/example/android_notification/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.android_notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_1_ID = "channel1";
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextMessage = findViewById(R.id.message);
+        editTextTitle = findViewById(R.id.title);
+        notificationManager = NotificationManagerCompat.from(this);
+
+
+    }
+    public void sendOnChannel1(View v){
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+        Notification notifcation = new NotificationCompat.Builder(this,CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_chat_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManager.notify(1,notifcation);
     }
 }
